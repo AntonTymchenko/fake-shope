92ed5347126399ba88c9d8d9f6cbfaa658c8bdfc
AntonTymchenko <76427403+AntonTymchenko@users.noreply.github.com>
2021-09-22 08:00:58 +0300

    well done

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+import ProductListing from "./containers/ProductListing";
+import Header from "./containers/Header";
+import "./App.css";
+import ProductDetails from "./containers/ProductDetails";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/" exact component={ProductListing} />
+          <Route path="/product/:productId" component={ProductDetails} />
+          <Redirect to="/" />
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
